0aa63cb946fb48c430be423ee1b068a4a3b8c6d2
beginner1986 <beginner1986@gmail.com>
2021-08-20 21:22:10 +0200

    Arrangement and seats plan printing implemented.

--- cinema/Cinema.java
+++ cinema/Cinema.java
@@ -2,7 +2,28 @@ package cinema;
 
 public class Cinema {
 
+    final static int rows = 7;
+    final static int cols = 8;
+    char seats[][] = new char[rows][cols];
+
     public static void main(String[] args) {
-        // Write your code here
+        printSeats();
+    }
+
+    static void printSeats() {
+        System.out.println("Cinema:");
+        System.out.print("  ");
+        for(int i=0; i<cols; i++) { {
+            System.out.printf("%d ", i + 1);
+        }}
+        System.out.println();
+
+        for(int i=0; i<rows; i++) {
+            System.out.printf("%d ", i + 1);
+            for(int j=0; j<cols; j++) {
+                System.out.print("S ");
+            }
+            System.out.println();
+        }
     }
 }
